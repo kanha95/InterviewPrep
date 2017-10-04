052e40916cd429a5cbcc303c6d2708ebedfaac27
Ruturaj Mohanty <kanha95@gmail.com>
2017-10-04 16:19:24 +0530

    SpiralOrderPrintMatrix

---
+++ SpiralOrderMatrix.java
@@ -0,0 +1,252 @@
+import java.io.*;
+import java.util.*;
+
+
+
+
+
+ class Main
+{
+   
+   public static void main(String args[]) throws Exception
+	{
+		InputReader sc=new InputReader(System.in);		
+		PrintWriter w=new PrintWriter(System.out);
+    
+         
+                int n=sc.nextInt();
+                
+                int[][] ar=new int[n][n]; 
+                
+                int top=0,left=0,bottom=n-1,right=n-1;
+                
+                int dir=0;int i=1;
+                while(top<=bottom && left<=right){
+                    
+                    if(dir==0){
+                        for(int k=left;k<=right;k++){
+                            ar[top][k]=i;
+                            i++;
+                        }
+                        top++;
+                        dir=1;
+                        
+                    }
+                    else if(dir==1){
+                        
+                        for(int k=top;k<=bottom;k++){
+                            ar[k][right]=i;
+                            i++;
+                        }
+                        right--;
+                        dir=2;
+                    }
+                    else if(dir==2){
+                        
+                        for(int k=right;k>=left;k--){
+                            ar[bottom][k]=i;
+                            i++;
+                        }
+                        bottom--;
+                        dir=3;
+                        
+                    }
+                    else if(dir==3){
+                        
+                        for(int k=bottom;k>=top;k--){
+                            ar[k][left]=i;
+                            i++;
+                        }
+                        left++;
+                        dir=0;
+                    }
+                    
+                    
+                }
+                
+                
+                for (int j = 0; j < n; j++) {
+                    for (int k = 0; k < n; k++) {
+                        System.out.print(ar[j][k]+" ");
+                    }System.out.println("");
+            }
+                
+ }
+ }
+	 class InputReader
+	{
+		private InputStream stream;
+		private byte[] buf = new byte[1024];
+		private int curChar;
+		private int numChars;
+		private SpaceCharFilter filter;
+		
+		public InputReader(InputStream stream)
+		{
+			this.stream = stream;
+		}
+		
+		public int read()
+		{
+			if (numChars==-1) 
+				throw new InputMismatchException();
+			
+			if (curChar >= numChars)
+			{
+				curChar = 0;
+				try 
+				{
+					numChars = stream.read(buf);
+				}
+				catch (IOException e)
+				{
+					throw new InputMismatchException();
+				}
+				
+				if(numChars <= 0)				
+					return -1;
+			}
+			return buf[curChar++];
+		}
+	 
+		public String nextLine()
+		{
+			BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+			String str = "";
+            try
+            {
+                str = br.readLine();
+            }
+            catch (IOException e)
+            {
+                e.printStackTrace();
+            }
+            return str;
+		}
+		public int nextInt()
+		{
+			int c = read();
+			
+			while(isSpaceChar(c)) 
+				c = read();
+			
+			int sgn = 1;
+			
+			if (c == '-') 
+			{
+				sgn = -1;
+				c = read();
+			}
+			
+			int res = 0;
+			do 
+			{
+				if(c<'0'||c>'9') 
+					throw new InputMismatchException();
+				res *= 10;
+				res += c - '0';
+				c = read();
+			}
+			while (!isSpaceChar(c)); 
+			
+			return res * sgn;
+		}
+		
+		public long nextLong() 
+		{
+			int c = read();
+			while (isSpaceChar(c))
+				c = read();
+			int sgn = 1;
+			if (c == '-') 
+			{
+				sgn = -1;
+				c = read();
+			}
+			long res = 0;
+			
+			do 
+			{
+				if (c < '0' || c > '9')
+					throw new InputMismatchException();
+				res *= 10;
+				res += c - '0';
+				c = read();
+			}
+			while (!isSpaceChar(c));
+				return res * sgn;
+		}
+		
+		public double nextDouble() 
+		{
+			int c = read();
+			while (isSpaceChar(c))
+				c = read();
+			int sgn = 1;
+			if (c == '-') 
+			{
+				sgn = -1;
+				c = read();
+			}
+			double res = 0;
+			while (!isSpaceChar(c) && c != '.') 
+			{
+				if (c == 'e' || c == 'E')
+					return res * Math.pow(10, nextInt());
+				if (c < '0' || c > '9')
+					throw new InputMismatchException();
+				res *= 10;
+				res += c - '0';
+				c = read();
+			}
+			if (c == '.') 
+			{
+				c = read();
+				double m = 1;
+				while (!isSpaceChar(c)) 
+				{
+					if (c == 'e' || c == 'E')
+						return res * Math.pow(10, nextInt());
+					if (c < '0' || c > '9')
+						throw new InputMismatchException();
+					m /= 10;
+					res += (c - '0') * m;
+					c = read();
+				}
+			}
+			return res * sgn;
+		}
+		
+		public String readString() 
+		{
+			int c = read();
+			while (isSpaceChar(c))
+				c = read();
+			StringBuilder res = new StringBuilder();
+			do 
+			{
+				res.appendCodePoint(c);
+				c = read();
+			} 
+			while (!isSpaceChar(c));
+			
+			return res.toString();
+		}
+	 
+		public boolean isSpaceChar(int c) 
+		{
+			if (filter != null)
+				return filter.isSpaceChar(c);
+			return c == ' ' || c == '\n' || c == '\r' || c == '\t' || c == -1;
+		}
+	 
+		public String next() 
+		{
+			return readString();
+		}
+		
+		public interface SpaceCharFilter 
+		{
+			public boolean isSpaceChar(int ch);
+		}
+	} 
