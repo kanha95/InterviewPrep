01d31b1bdcae1c2b0afb4b30f0fbdfb49a014cdf
Ruturaj Mohanty <kanha95@gmail.com>
2017-10-06 02:37:39 +0530

    FloydCycleDetectionAlgorithm

---
+++ FloydCycle.java
@@ -0,0 +1,252 @@
+import java.io.*;
+import java.util.*;
+
+
+
+class node{
+    int data;
+    node next;
+    
+    node(int data){
+        this.data=data;
+        next=null;
+    }
+    
+    
+}
+
+
+ class Main
+{
+ 
+     
+     static void floydCycle(node head){
+         
+         node slowptr=head,fastptr=head;
+         boolean b=false;
+         
+         do{   
+             slowptr=slowptr.next;
+             fastptr=fastptr.next.next;
+             if(slowptr==fastptr){
+                 b=true;
+                 break;
+             }
+         }
+         while(slowptr!=null && fastptr!=null);
+         
+         if(b){
+             System.out.println("Cycle Exists");
+         }
+       
+         slowptr=head;
+   
+         while(slowptr!=fastptr){
+             slowptr=slowptr.next;
+             fastptr=fastptr.next;
+         }
+         System.out.println("Starting point of Cycle "+slowptr.data);
+         
+     }
+     
+     
+     
+   public static void main(String args[]) throws Exception
+	{
+		InputReader sc=new InputReader(System.in);		
+		PrintWriter w=new PrintWriter(System.out);
+    
+                
+       node head=new node(1);
+       head.next=new node(2);
+       head.next.next=new node(3);
+       node temp=head.next.next;
+       head.next.next.next=new node(4);
+       head.next.next.next.next=new node(5);
+       head.next.next.next.next.next=new node(6);
+       head.next.next.next.next.next.next=temp;
+       
+       
+       
+       floydCycle(head);
+           
+           
+ }
+ }
+	 class InputReader
+	{
+		private InputStream stream;
+		private byte[] buf = new byte[1024];
+		private int curChar;
+		private int numChars;
+		private SpaceCharFilter filter;
+		
+		public InputReader(InputStream stream)
+		{
+			this.stream = stream;
+		}
+		
+		public int read()
+		{
+			if (numChars==-1) 
+				throw new InputMismatchException();
+			
+			if (curChar >= numChars)
+			{
+				curChar = 0;
+				try 
+				{
+					numChars = stream.read(buf);
+				}
+				catch (IOException e)
+				{
+					throw new InputMismatchException();
+				}
+				
+				if(numChars <= 0)				
+					return -1;
+			}
+			return buf[curChar++];
+		}
+	 
+		public String nextLine()
+		{
+			BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+			String str = "";
+            try
+            {
+                str = br.readLine();
+            }
+            catch (IOException e)
+            {
+                e.printStackTrace();
+            }
+            return str;
+		}
+		public int nextInt()
+		{
+			int c = read();
+			
+			while(isSpaceChar(c)) 
+				c = read();
+			
+			int sgn = 1;
+			
+			if (c == '-') 
+			{
+				sgn = -1;
+				c = read();
+			}
+			
+			int res = 0;
+			do 
+			{
+				if(c<'0'||c>'9') 
+					throw new InputMismatchException();
+				res *= 10;
+				res += c - '0';
+				c = read();
+			}
+			while (!isSpaceChar(c)); 
+			
+			return res * sgn;
+		}
+		
+		public long nextLong() 
+		{
+			int c = read();
+			while (isSpaceChar(c))
+				c = read();
+			int sgn = 1;
+			if (c == '-') 
+			{
+				sgn = -1;
+				c = read();
+			}
+			long res = 0;
+			
+			do 
+			{
+				if (c < '0' || c > '9')
+					throw new InputMismatchException();
+				res *= 10;
+				res += c - '0';
+				c = read();
+			}
+			while (!isSpaceChar(c));
+				return res * sgn;
+		}
+		
+		public double nextDouble() 
+		{
+			int c = read();
+			while (isSpaceChar(c))
+				c = read();
+			int sgn = 1;
+			if (c == '-') 
+			{
+				sgn = -1;
+				c = read();
+			}
+			double res = 0;
+			while (!isSpaceChar(c) && c != '.') 
+			{
+				if (c == 'e' || c == 'E')
+					return res * Math.pow(10, nextInt());
+				if (c < '0' || c > '9')
+					throw new InputMismatchException();
+				res *= 10;
+				res += c - '0';
+				c = read();
+			}
+			if (c == '.') 
+			{
+				c = read();
+				double m = 1;
+				while (!isSpaceChar(c)) 
+				{
+					if (c == 'e' || c == 'E')
+						return res * Math.pow(10, nextInt());
+					if (c < '0' || c > '9')
+						throw new InputMismatchException();
+					m /= 10;
+					res += (c - '0') * m;
+					c = read();
+				}
+			}
+			return res * sgn;
+		}
+		
+		public String readString() 
+		{
+			int c = read();
+			while (isSpaceChar(c))
+				c = read();
+			StringBuilder res = new StringBuilder();
+			do 
+			{
+				res.appendCodePoint(c);
+				c = read();
+			} 
+			while (!isSpaceChar(c));
+			
+			return res.toString();
+		}
+	 
+		public boolean isSpaceChar(int c) 
+		{
+			if (filter != null)
+				return filter.isSpaceChar(c);
+			return c == ' ' || c == '\n' || c == '\r' || c == '\t' || c == -1;
+		}
+	 
+		public String next() 
+		{
+			return readString();
+		}
+		
+		public interface SpaceCharFilter 
+		{
+			public boolean isSpaceChar(int ch);
+		}
+	} 
